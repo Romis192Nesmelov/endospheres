646e23fafb9649c1ed90db30981b44a686f81640
Romis <romis.nesmelov@gmail.com>
2018-08-04 19:23:31 +0300

    trying to fix FF bugs

--- public/js/landing.js
+++ public/js/landing.js
@@ -204,9 +204,9 @@ function nextSlide() {
             setTimeout(function() {
                 breakingAnim(100, 0, maskInvert, 'y', '%', -1, 1.055, function () {
                     $('#background-image').attr('xlink:href',imageSrc).css('opacity',1);
-                    maskInvert.attr('y','100%');
-                    decadesCont.attr('y','0%');
-                    unitsCont.attr('y','0%');
+                    // maskInvert.attr('y','100%');
+                    // decadesCont.attr('y','0%');
+                    // unitsCont.attr('y','0%');
                     // removeVideo();
                     // showMouse();
                 });
