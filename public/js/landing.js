bbc81ba76621f426962d748eaac596887c814ddd
Romis <romis.nesmelov@gmail.com>
2018-08-04 19:28:07 +0300

    trying to fix FF bugs

--- public/js/landing.js
+++ public/js/landing.js
@@ -203,7 +203,8 @@ function nextSlide() {
 
             setTimeout(function() {
                 breakingAnim(100, 0, maskInvert, 'y', '%', -1, 1.055, function () {
-                    $('#background-image').attr('xlink:href',imageSrc).css('opacity',1);
+                    $('#background-image').attr('xlink:href',imageSrc);
+                    $('#background-image').css('opacity',1);
                     maskInvert.attr('y','100%');
                     decadesCont.attr('y','0%');
                     unitsCont.attr('y','0%');
