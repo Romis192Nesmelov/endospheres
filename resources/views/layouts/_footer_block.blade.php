f10ab99766f0d759423d4f433a204d7edddbed3a
Romis <romis.nesmelov@gmail.com>
2018-08-18 07:32:14 +0300

    many fixes

--- resources/views/layouts/_footer_block.blade.php
+++ resources/views/layouts/_footer_block.blade.php
@@ -28,13 +28,9 @@
 <div id="copyright-line">
     <div class="container">
         <div class="copyright hidden-xs">{{ trans('content.copyright') }}</div>
-
-        <div class="soc-icon"><a href="#"><img src="{{ asset('images/vk.png') }}" /></a></div>
+        <div class="soc-icon" style="margin-left: 20px;"><a href="#"><img src="{{ asset('images/vk.png') }}" /></a></div>
         <div class="soc-icon"><a href="#"><img src="{{ asset('images/youtube.png') }}" /></a></div>
         <div class="soc-icon"><a href="#"><img src="{{ asset('images/fb.png') }}" /></a></div>
         <div class="soc-icon"><a href="#"><img src="{{ asset('images/insta.png') }}" /></a></div>
-
-
-
     </div>
 </div>
