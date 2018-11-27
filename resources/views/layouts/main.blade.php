d87509941215476d7bc35d5c735c79a937b7e8e6
Romis <romis.nesmelov@gmail.com>
2018-11-27 16:50:34 +0300

    added yandex-metrix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -48,6 +48,35 @@
 </head>
 <body>
 
+<!— Yandex.Metrika counter —>
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter44462833 = new Ya.Metrika({
+                    id:44462833,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/44462833" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!— /Yandex.Metrika counter —>
+
 @include('layouts._modal_block',['id' => 'message', 'message' => trans('content.thanks_for_your_message')])
 @include('layouts._feedback_modal_block')
 
