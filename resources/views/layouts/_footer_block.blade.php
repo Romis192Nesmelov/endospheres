2d81a7919a17fc2c8e82899c73b375dc17554f17
Romis <romis.nesmelov@gmail.com>
2019-02-22 10:35:30 +0300

    changed footer hrefs

--- resources/views/layouts/_footer_block.blade.php
+++ resources/views/layouts/_footer_block.blade.php
@@ -63,9 +63,9 @@
         <div class="copyright hidden-xs">{{ trans('content.copyright') }}</div>
         <div class="pull-right">
             <div class="soc-icon"><a href="https://vk.com/endospherestherapyrussia"><img src="{{ asset('images/vk.png') }}" /></a></div>
-            <div class="soc-icon"><a href="https://www.youtube.com/user/FenixGroupVideo?ob=5"><img src="{{ asset('images/youtube.png') }}" /></a></div>
+            <div class="soc-icon"><a href="https://www.youtube.com/channel/UCqKzf0mlFRozg0dAtchL3yg"><img src="{{ asset('images/youtube.png') }}" /></a></div>
             <div class="soc-icon"><a href="https://www.facebook.com/EndospheresTherapyRussia"><img src="{{ asset('images/fb.png') }}" /></a></div>
-            <div class="soc-icon"><a href="https://www.instagram.com/official_endospheres_russia/"><img src="{{ asset('images/insta.png') }}" /></a></div>
+            <div class="soc-icon"><a href="https://www.instagram.com/endospheresrussia_official/"><img src="{{ asset('images/insta.png') }}" /></a></div>
         </div>
     </div>
 </div>
