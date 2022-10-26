78a16a59fc191f36a2be95a67b9a843df6fedb0e
Romis <romis.nesmelov@gmail.com>
2022-10-26 12:30:18 +0300

    return feedback-form to contacts

--- resources/views/layouts/_footer_block.blade.php
+++ resources/views/layouts/_footer_block.blade.php
@@ -1,4 +1,4 @@
-@if (isset($data['chapter']) && ($data['chapter']->id == 1 || $data['chapter']->id == 10))
+@if (isset($data['chapter']) && ($data['chapter']->id == 1 || $data['chapter']->id == 11))
     <div class="container">
         <h2>{{ trans('content.answer_question') }}</h2>
     </div>
